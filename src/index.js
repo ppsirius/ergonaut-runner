3a1f454d68a4586af48f9e34b361115bb3954be5
Piotr Pupczyk <ppsirius@gmail.com>
2020-06-08 12:57:17 +0200

    update

--- src/index.js
+++ src/index.js
@@ -6,6 +6,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Ranking from "./ranking";
 import Popup from "./popup";
+import clp from "console-log-plus";
+
+setTimeout(() => {
+  clp({
+    color: "#fff",
+    background: "#000",
+    message: "jakbyco.com",
+  });
+}, 500);
 
 ReactDOM.render(
   <div>
